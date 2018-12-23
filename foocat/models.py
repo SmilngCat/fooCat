e452ae3b9486422326c5c7c4864131edb2bc460c
SmilngCat <leiyadong800@icloud.com>
2018-12-23 16:30:56 +0800

    database changed

--- foocat/models.py
+++ foocat/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Device(models.Model):
+    did = models.CharField(max_length=40)
+    name = models.CharField(max_length=60, blank=True, null=True)
+
+    class Meta:
+        db_table = 'device'
